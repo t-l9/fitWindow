18b8081374b3927a3372fa4c33d58077883ef5e7
Tim Lauter <timothy.lauter@gmail.com>
2015-06-08 11:26:28 -0400

    first commit

---
+++ fitWindow.js
@@ -0,0 +1,29 @@
+(function() {
+
+    this.fitWindow = function() {
+
+        if (arguments[0] && typeof arguments[0] === "string") {
+            this.section = arguments[0];
+        } else {
+            console.log('Error: Please enter an Element ID as a string. e.g: var foo = new fitWindow(\'hero\')');
+        };
+
+        this.resizeSection = function() {
+            hero = document.getElementById(this.section);
+            hero.style.height = window.innerHeight;
+            hero.style.width = window.innerWidth;
+        };
+
+        this.init = function(){
+            this.resizeSection();
+            var self = this;
+            window.addEventListener( 'resize', function(){
+                self.resizeSection();
+
+            });
+        };
+
+        this.init();
+    };
+
+})();
